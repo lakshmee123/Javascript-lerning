ac3d5d657ef0803e258465d59b7077cf4d6be8e5
lakshmee123 <91306599+lakshmee123@users.noreply.github.com>
2023-11-15 10:46:47 +0000

    done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId=123456;
+let accountEmail="laky@gmail.com";
+var accountPassword ="1234567";
+//accountPassword="2";
+console.table([accountPassword,accountEmail]);
+//prefer not to use var because of issue in block scop and funtional scop//
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newrversion
+ //alert (3+3) // we are using node js , not browser
+ //code readalibity should high
+ let name="rishi";
+ let a=1
+ let islogin=false
+// primitive Data Types
+ //number=>2 to power 53
+ //bigint
+ //string=>""
+ //boolian=>true/false
+ //null=>standalon
+ //undefine=>
+ //symbol=>uniq
+
+ // object
+ console.log(typeof undefined);//undefined
+ console.log(typeof null);//object
+ 
