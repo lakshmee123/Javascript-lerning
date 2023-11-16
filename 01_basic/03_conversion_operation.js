d82043f25031af61de423ce987094a6ecb8e2133
lakshmee123 <91306599+lakshmee123@users.noreply.github.com>
2023-11-16 11:56:04 +0000

    conversion of data types

---
+++ 01_basic/03_conversion_operation.js
@@ -0,0 +1,21 @@
+let score=null;
+//let score="33";
+
+console.log(typeof score);
+let valueInNumber=Number(score);
+//console.log(valueInNumber);
+
+//"33"=>"33"
+//"33abc"=>NaN
+//"true"=>1; false=>0;
+
+let islogedIn=1
+let boolianisLoggedIn=Boolean(islogedIn);
+//console.log(boolianisLoggedIn);
+
+//1=>true 0=>false
+//""=>false
+//"hitesh"=>true
+let som=99;
+let somString=String(som);
+//console.log(typeof somString);
